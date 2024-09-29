2ba49c353fd37b13094592c1e1571d37681ff974
VigKillian <viguier.killian05@gmail.com>
2024-09-29 13:59:22 +0200

    Modification du sprite selon la direction du chien

--- src/scenes/chien/chien.gd
+++ src/scenes/chien/chien.gd
@@ -2,18 +2,25 @@ extends RigidBody2D
 
 var speed := 200.0
 var is_moving := false
+var prev_position = Vector2()
 
 
 @onready var _animated_sprite = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	prev_position = position
 	_animated_sprite.play("fixe")
-	pass # Replace with function body.
+	 # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if linear_velocity.x < 0:
+		_animated_sprite.scale.x = -2
+	if linear_velocity.x > 0:
+		_animated_sprite.scale.x = 2
+	prev_position = position
 	_animated_sprite.play("court")
 	var ax = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	apply_central_force(ax* speed)
