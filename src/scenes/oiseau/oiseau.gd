a820ecd5694b448f7e706740a0e2e5ff46add7de
binaryHips <tom.zinck@etu.umontpellier.fr>
2024-11-08 18:05:24 +0100

    gt

--- src/scenes/oiseau/oiseau.gd
+++ src/scenes/oiseau/oiseau.gd
@@ -1,32 +1,52 @@
 extends AnimatedSprite2D
 
 var coeffs = []
+var sens = 1
 @onready var _animated_sprite = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position.x = -500
-	for i in randi_range(1, 3):
-		coeffs.append(randf_range(-1, 1))
+	sens = [1, -1].pick_random()
+	position.x = -500 * sens
+	coeffs = []
+	coeffs.append(randf_range(-100, 100)) # constant
+	
+	coeffs.append(randf_range(-1, 1)) # deg 1
+	
+	coeffs.append(randf_range(-0.001, 0.001)) # deg 2
+	
+	coeffs.append(randf_range(-0.00001, 0.00001)) # deg 3
 	print(coeffs)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(position)
-	const h = 0.001
-	var diff = abs(f(position.x)-f(position.x+h))/h
-	if(diff != 0):
-		position.x += delta*diff*1000
-	else:
-		position.x += delta*10
-	position.y = f(position.x)
-	print(diff)
+	print(global_position)
+	#const h = 0.001
+	const speed = 60;
+	#var diff = abs(f(global_position.x+h)-f(global_position.x)/h)
+
+	global_position.x += delta*speed * sens
+
+	global_position.y = f(global_position.x)
 	pass
 
 func f(x: float) -> float:
-	var res = 0.
+	var res = 0
 	for i in len(coeffs):
 		res += coeffs[i]*(x ** i)
 	return res
+
+func _draw():
+	if (false):
+		for i in range(-500, 500):
+			draw_line(
+				Vector2(i, f(i)) - position,
+				Vector2(i+1, f(i+1)) - position,
+				Color.WEB_PURPLE,
+				2.0)
+
+func _input( event ):
+	if event.is_action("ui_accept"):
+		_ready()
