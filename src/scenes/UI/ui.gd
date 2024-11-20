625f5f3f5382e6757319ddc891ad1bb31d469a86
binaryHips <tom.zinck@etu.umontpellier.fr>
2024-11-20 16:29:58 +0100

    bvnn

--- src/scenes/UI/ui.gd
+++ src/scenes/UI/ui.gd
@@ -18,19 +18,7 @@ func _on_h_slider_value_changed(value: float) -> void:
 
 var tween:Tween
 func _on_mouse_entered() -> void:
-	
-	tween = get_tree().create_tween()
-	tween.tween_property(
-		$Panel,
-		"position",
-		Vector2(0, -100),
-		0.6
-	).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
-	
-	tween.play()
 
-
-func _on_mouse_exited() -> void:
 	
 	tween = get_tree().create_tween()
 	tween.tween_property(
@@ -40,4 +28,20 @@ func _on_mouse_exited() -> void:
 		0.6
 	).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
 	tween.play()
+
+
+func _on_mouse_exited() -> void:
+	tween = get_tree().create_tween()
+	tween.tween_property(
+		$Panel,
+		"position",
+		Vector2(0, -100),
+		0.6
+	).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
 	
+	tween.play()
+	
+
+
+func _on_finished_presse() -> void:
+	Gamemaster.end()
