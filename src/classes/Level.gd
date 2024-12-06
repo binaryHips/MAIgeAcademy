f7d7f7764de1a5f3b2f064b2df8d079acf46417b
binaryHips <tom.zinck@etu.umontpellier.fr>
2024-12-06 13:57:22 +0100

    sertdgfh

--- src/classes/Level.gd
+++ src/classes/Level.gd
@@ -11,8 +11,6 @@ func _ready() -> void:
 
 
 
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- src/classes/Strategy.gd
+++ src/classes/Strategy.gd
@@ -1,6 +1,7 @@
 extends Resource
 class_name StudentStrategy
 
+static var candy_count:int = 0
 func _decideGoal(brain:Brain, percept:Dictionary):
 	push_error("_decideGoal in strategy not implemented")
 	
