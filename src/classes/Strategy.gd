467aeef9462fcf89503f77a32754ac35291dfe2f
VigKillian <viguier.killian05@gmail.com>
2024-12-07 10:32:27 +0100

    add states in strategy

--- src/classes/Strategy.gd
+++ src/classes/Strategy.gd
@@ -23,12 +23,27 @@ func _decideGoal(brain:Brain, percept:Dictionary): #percept contient positionPla
 						return false
 				}
 				brain.override_goal(dico)
-		#"goToCandy":
+		"goToCandy":
+			if(percept["candies_by_distance"].size() == 0):
+				brain.override_state("goBackToPlace")
+				var dico = {
+					"name": "goBackToPlace",
+					"move_target": brain.base_pos,
+					"time_remaining": -1,
+					"goal_check": func(percept:Dictionary) :
+						var dist = brain.body.global_position.distance_to(brain.base_pos)
+						if(dist <= 1.0):
+							return true
+						return false
+				}
+				brain.override_goal(dico)
 			
 			
 	push_error("_decideGoal in strategy not implemented")
 	
 func _act(brain:Brain, percept:Dictionary):
-	if(brain.has_state("idle")):
+	if(brain.has_state("goToCandy")):
+		var brain_goal = brain.goals.filter(func(dico):dico["name"] == "goToCandy")
+		#fonction pour bouger vers le bonbon
 		
 	push_error("_act in strategy not implemented")
