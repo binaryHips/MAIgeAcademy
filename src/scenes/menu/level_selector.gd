8490e411cbfe2f765e29348a07e39be19058d714
tom <tomben01@laposte.net>
2024-12-15 16:41:41 +0100

    menuuu

--- src/scenes/menu/level_selector.gd
+++ src/scenes/menu/level_selector.gd
@@ -5,16 +5,25 @@ var levels = [
 	
 	{
 		"index": 0,
-		"name": "class outside!",
+		"name": "recess",
+		"scene": preload("res://src/levels/level0/Level0.tscn"),
 		"map": preload("res://resources/images/coursMagie.png"),
-		"max_students": 10
+		"max_students": 13
 	},
 	{
 		"index": 1,
 		"name": "snow recess",
+		"scene": preload("res://src/levels/level0/Level1.tscn"),
 		"map": preload("res://resources/images/cour_enneigee.png"),
 		"max_students": 15
-	}
+	},
+	{
+		"index": 2,
+		"name": "class outside!",
+		"scene": preload("res://src/levels/level0/Level2.tscn"),
+		"map": preload("res://resources/images/coursMagie.png"),
+		"max_students": 13
+	},
 ]
 
 # Called when the node enters the scene tree for the first time.
@@ -26,10 +35,19 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func update_level():
+	Gamemaster.current_level_data = levels[current_selected_level]
+	$"../VBoxContainer".update_values()
+	$level_name.text = "[center]" + Gamemaster.current_level_data["name"]
+	$Panel/TextureRect.texture = Gamemaster.current_level_data["map"]
+
 var current_selected_level:int = 0
 func _on_next_level_pressed() -> void:
 	current_selected_level = (current_selected_level+1) % levels.size()
-
+	$"../../../sonMenu2".play()
+	update_level()
 
 func _on_previous_level_pressed() -> void:
-	pass # Replace with function body.
+	current_selected_level = (current_selected_level-1 +levels.size()) % levels.size()
+	$"../../../sonMenu2".play()
+	update_level()
